2355ad0a451187abb8629b5d875409a175c825b7
Katt <Raxxyn@gmail.com>
2022-12-18 21:42:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace concatenation_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new[] { 0, 1, 2, 3, 4 };
+            var y = new[] { 4, 5 };
+
+            var joinedArray = Conkatt(x, y);
+            PrintArray(joinedArray);
+        }
+
+        private static void PrintArray(int[] array, [System.Runtime.CompilerServices.CallerArgumentExpression("array")] string arrayName = "")
+        {
+            Console.WriteLine($"{arrayName} = [{string.Join(", ", array)}]");
+        }
+
+        private static int[] Conkatt(int[] x, int[] y)
+        {
+            var z = new int[x.Length + y.Length];
+            
+            for (var i = 0; i < x.Length; i++)
+                z[i] = x[i];
+
+            for (var i = 0; i < y.Length; i++)
+                z[i + x.Length] = y[i];
+
+            return z;
+        }
+    }
+}
